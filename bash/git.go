198499108f18889834ebcd2ad03a7199a4aa383b
sandman <bmsandoval@gmail.com>
2020-02-25 15:57:28 -0700

    more

--- bash/git.go
+++ bash/git.go
@@ -5,6 +5,6 @@ func GitFetchRecurse() string { return "git fetch --recurse-submodules" }
 func GitPullRecurse() string { return "git pull --recurse-submodules" }
 func GitStash() string { return "git stash" }
 func GitStashRecurse() string { return "git submodule foreach 'git stash'" }
-func GitResetHard() string { return "git reset --hard" }
-func GitResetHardRecurse() string { return "git submodule foreach 'git reset --hard'" }
+func GitResetHard() string { return "git reset HEAD --hard" }
+func GitResetHardRecurse() string { return "git submodule foreach 'git reset HEAD --hard'" }
 

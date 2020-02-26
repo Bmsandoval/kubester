bb7769cd9c1d227f10c392d9d878f33bb6fe8219
sandman <bmsandoval@gmail.com>
2020-02-25 17:01:39 -0700

    removing remaining references to medic

--- cmd/root.go
+++ cmd/root.go
@@ -30,7 +30,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-  Use:   "medic",
+  Use:   "kubester",
   Short: "A brief description of your application",
   Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -59,7 +59,7 @@ func init() {
   // Cobra supports persistent flags, which, if defined here,
   // will be global for your application.
 
-  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.medic.yaml)")
+  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kubester.yaml)")
 
 
   // Cobra also supports local flags, which will only run
